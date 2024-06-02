ee187b8aa9598464ffc199592029b686ce786bd2
Manuel <90015127+Manuel-Progaska@users.noreply.github.com>
2024-06-02 23:43:36 +0000

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,20 @@ def tasas(nombre:str, start:str=None, end:str=None) -> pd.DataFrame:
 
 st.title('Explorador de Tasas Banco Central Chile')
 
+## FUNCIONES
+
+@st.cache
+def tasas(nombre:str, start:str=None, end:str=None) -> pd.DataFrame:
+
+    # datos banco central
+    bc = BC_Data()
+
+    if nombre == 'Swap Camara CLP':
+
+        df: pd.DataFrame = bc.tasa_swap_clp(start=start, end=end)
+
+        return df
+
 # tasas
 df : pd.DataFrame = tasas(nombre='Swap Camara CLP')
 st.write(df)
