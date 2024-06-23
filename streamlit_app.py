2082195d80b37499c3028b0ce525d42c885eff3b
Manuel <Progaska.m@gmail.com>
2024-06-23 10:53:35 -0400

    cambio en app

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ df = bc.load_type(type='SWAP_CLP')
 
 st.title('EXPLORADOR DE TASAS BANCO CENTRAL CHILE')
 st.header('Tasa Swap Promedio Camara CLP')
-st.dataframe(df)
+st.dataframe(df, use_container_width=True)
