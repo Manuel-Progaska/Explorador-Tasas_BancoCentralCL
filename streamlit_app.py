b5f720d6eabb32e225a026a2a8033aa2476cd205
Manuel <Progaska.m@gmail.com>
2024-07-02 23:50:34 -0400

    cambios en streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,46 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import pandas as pd
+from functools import partial
 from main import BC_Data
 
+# page confict
+st.set_page_config(layout="wide")
 
+
+# incialize BC_Data
 bc = BC_Data()
-df = bc.load_type(type='SWAP_CLP')
 
+## FUNCTIONS
+@st.cache_data
+def swaps_cl() -> pd.DataFrame:
+    """_summary_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    
+    
+    df: pd.DataFrame = bc.load_type(type='SWAP_CLP')
+    
+    return df
+
+## DATOS
+swp_cl = swaps_cl() 
+
+## LAYOUT
+
+# sidebar menu
+with st.sidebar:
+    selectted = option_menu(
+        menu_title='Tasas',
+        options=['Swap CLP', 'SWAP UF']
+    )
+
+# sidebar
+st.sidebar.title('Parametros')
+
+# page
 st.title('EXPLORADOR DE TASAS BANCO CENTRAL CHILE')
-st.header('Tasa Swap Promedio Camara CLP')
-st.dataframe(df, use_container_width=True)
+st.header('Tasas Históricas SPC-CLP')
+st.dataframe(swp_cl, use_container_width=True, hide_index=True)
